a8edd980f81e0b1424239da15ca18c84120345bf
BenMusak <56428534+BenMusak@users.noreply.github.com>
2021-03-31 15:23:59 +0200

    Replacing highlights working, needs color correction.

--- BenjaminFunctions.py
+++ BenjaminFunctions.py
@@ -2,6 +2,45 @@ import cv2
 import numpy as np
 
 
-def replaceHighlights(main_img, spec_img):
-    main_img_spec = np.where((main_img[:, :, 0] == 255) & (main_img[:, :, 1] == 255) & (main_img[:, :, 2] == 255))
-    cv2.imshow("spec", main_img_spec)
+def replaceHighlights(main_img, spec_img, limit):
+    '''
+    This functions replaces the highlights from a main picture with the pixels from a specular image pixels
+
+    :param main_img: The image of which will get the pixels replaced with the specular image
+    :param spec_img: The image of which will be used to replace the pixels of the main image
+    :param limit: The limits of a pixel value before it is classified as a specular highlight
+    :return: The image that has the highlights replaced
+    '''
+
+    # Copy
+    img_main_cop = np.copy(main_img)
+
+    # Isolate the areas where the color is white
+    main_img_spec = np.where((img_main_cop[:, :, 0] >= limit) & (img_main_cop[:, :, 1] >= limit) &
+                             (img_main_cop[:, :, 2] >= limit))
+
+    print(main_img_spec)
+
+    # Replace pixels with
+    img_main_cop[main_img_spec] = spec_img[main_img_spec]
+
+    cv2.imshow("spec", img_main_cop)
+
+
+def equalizeColoredImage(img):
+    '''
+    This function returns the equalized image of a color image using YUV
+
+    :param img: The image to equalize
+    :return: Equalized image
+    '''
+    # Turn into YUV
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+
+    # equalize the histogram of the Y channel
+    img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
+
+    # convert the YUV image back to RGB format
+    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+
+    return img_output
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import cv2
 
-from Kasperfunctions import SURFalignment, meanEdgeRGB, doClaheLAB2, limitLchannel, crop, doClaheLAB1, resizeImg
-from BenjaminFunctions import replaceHighlights
+from Kasperfunctions import crop, resizeImg
+from BenjaminFunctions import replaceHighlights, equalizeColoredImage
 
 # load image
-fisk = cv2.imread('fishpics/direct2pic\\GOPR1591.JPG', 1)
-top = cv2.imread('fishpics/direct2pic\\GOPR1591.JPG', 1)
-top = crop(top, 650, 500, 1000, 3000)
-top_re = resizeImg(top, 30)
+left = cv2.imread('fishpics/direct2pic\\GOPR1591.JPG', 1)
+left_crop = crop(left, 650, 500, 1000, 3000)
+left_re = resizeImg(left_crop, 30)
+left_eq = equalizeColoredImage(left_re)
 
-bund = cv2.imread('fishpics/direct2pic\\GOPR1590.JPG', 1)
+right = cv2.imread('fishpics/direct2pic\\GOPR1590.JPG', 1)
+right_crop = crop(right, 650, 500, 1000, 3000)
+right_re = resizeImg(right_crop, 30)
+right_eq = equalizeColoredImage(right_re)
+
+replaceHighlights(left_eq, right_eq, 220)
 
 # display image
-cv2.imshow("ResultHLS", img)
+cv2.imshow("left", left_eq)
+cv2.imshow("right", right_eq)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
