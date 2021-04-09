c1c2c09c074d221d84f7c1ad615e9a8c07011f4f
kasperFuglsangGrontved <kgrant19@student.aau.dk>
2021-04-09 10:48:07 +0200

    import pictures correctly

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from Kasperfunctions import crop, resizeImg
 from BenjaminFunctions import replaceHighlights, equalizeColoredImage
 
 # load image
-left = cv2.imread('fishpics/direct2pic\\GOPR1591.JPG', 1)
+left = cv2.imread('fishpics/direct2pic/GOPR1591.JPG', 1)
 left_crop = crop(left, 650, 500, 1000, 3000)
 left_re = resizeImg(left_crop, 30)
 left_eq = equalizeColoredImage(left_re)
 
-right = cv2.imread('fishpics/direct2pic\\GOPR1590.JPG', 1)
+right = cv2.imread('fishpics/direct2pic/GOPR1590.JPG', 1)
 right_crop = crop(right, 650, 500, 1000, 3000)
 right_re = resizeImg(right_crop, 30)
 right_eq = equalizeColoredImage(right_re)
