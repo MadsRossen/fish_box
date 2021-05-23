0a63424468aaa74bc4c1a4384259b48ae2225335
kasperfg16 <56404081+kasperfg16@users.noreply.github.com>
2021-05-23 17:09:05 +0200

    Checked for assignment

--- main_harris-corner-detection.py
+++ main_harris-corner-detection.py
@@ -8,44 +8,13 @@ import scipy.io
 import copy
 from matplotlib import pyplot as plt
 
-'''
-# Sort 2D numpy array by 1nd Column from high to low
-CornerList = [[3, 20],[1, 4],[2, 1],[5, 1]]
-print(CornerList)
-
-imgspoints = np.zeros((2,4,2))
-
-CornerListSortedX = sorted(CornerList, key=lambda x: x[0], reverse=True)
-
-CornerListSortedXY = []
-
-array = []
-
-# Sort 2D numpy array by 2nd Column from high to low, but only for checkerboard_size[1] at a time
-checkerboard_size = 2
-for n in range(int(4 / checkerboard_size)):
-    start = n * checkerboard_size
-    stop = start + checkerboard_size
-    nextRows = CornerListSortedX[start:stop]
-    nextRows_sorted = sorted(nextRows, key=lambda x: x[1], reverse=True)
-    for x in range(len(nextRows_sorted)):
-        CornerListSortedXY.append(nextRows_sorted[x])
-
-array.append(CornerListSortedXY)
-array.append(CornerListSortedXY)
-
-np_array = np.array(array)
-np_array_switch = np.moveaxis(np_array, 0, -1)
-
-scipy.io.savemat('np_array_switch.mat', mdict={'np_array_switch': np_array_switch})
-'''
-# Code starts here
-
 img_dir = "checkerboard_pics"  # Enter Directory of all images
 data_path = os.path.join(img_dir, '*JPG')
 files = glob.glob(data_path)
 filename_Imgs = []
 checkerboard_Imgs = []
+
+# Load all images
 for f1 in files:
     img = cv.imread(f1, 0)
     filename_Imgs.append(img)
@@ -67,11 +36,7 @@ enoughPointimgs = 0  # Number of images used
 # List of booleans for images that are used and not used
 usedimgsNum = []
 
-# Til test af cornerbilleder set til true eller false:
-test = False
-# Få corner coordinate set til true eller false:
-CornerCor = True
-
+# Detect corners in all images and save the coordinates for all the corners in all the images
 for n in range(len(checkerboard_Imgs)):
     if n == 0:
         CornerListSortedXY_stacked = []
@@ -92,7 +57,7 @@ for n in range(len(checkerboard_Imgs)):
     # Partial differentiation hvor ** = ^2
     Iy, Ix = np.gradient(blur)
 
-    # Repræsentation af M matricen
+    # Representation of the M matrix
     Ixx = Ix ** 2
     Ixy = Iy * Ix
     Iyy = Iy ** 2
@@ -104,32 +69,30 @@ for n in range(len(checkerboard_Imgs)):
     for y in range(offset, y_size):
         for x in range(offset, x_size):
 
-            # Variabler for det window den kører over hver windowSize
+            # Variables for the window
             start_x = x - offset
             end_x = x + offset + 1
             start_y = y - offset
             end_y = y + offset + 1
 
-            # Laver det window den køre med
+            # Create the window
             windowIxx = Ixx[start_y: end_y, start_x: end_x]
             windowIxy = Ixy[start_y: end_y, start_x: end_x]
             windowIyy = Iyy[start_y: end_y, start_x: end_x]
 
-            # Summed af det enkelte window
+            # Sum of the window
             Sxx = windowIxx.sum()
             Sxy = windowIxy.sum()
             Syy = windowIyy.sum()
 
-            # Beregner determinanten og dirgonalen(tracen) for mere info --> se Jacobian formula
+            # Calculate the determinant and the trace for the M matrix. More info --> see Jacobian formula
             det = (Sxx * Syy) - (Sxy ** 2)
             trace = Sxx + Syy
 
-            # finder r for harris corner detection equation
+            # Finds the R value for the harris corner detection equation
             r = det - k * (trace ** 2)
 
-            if bool(test):
-                CornerCoordinate.append([x, y, Ix[y, x], Iy[y, x], r])
-
+            # Filter out the coordinates that is definitely not corners
             if r > threshold:
                 nul.itemset((y, x), 255)
                 img.itemset((y, x, 0), 0)
@@ -137,107 +100,75 @@ for n in range(len(checkerboard_Imgs)):
                 img.itemset((y, x, 2), 0)
 
     # Create a list of corner coordinates
-    if bool(CornerCor):
-        print("Starting GrassFire . . .")
-        Objects = gf.GrassFire(nul)
+    print("Starting GrassFire . . .")
+    Objects = gf.GrassFire(nul)
 
-        # Sort the list by the mass of objects
-        print("Number of objects: ", len(Objects))
-        ObjectsH = sorted(Objects, key=len, reverse=True)
+    # Sort the list by the mass of objects
+    print("Number of objects: ", len(Objects))
+    ObjectsH = sorted(Objects, key=len, reverse=True)
 
-        # Corner list for all corner candidates
-        CornerList = []
-        CornerListSortedXY = []
+    # Corner list for all corner candidates
+    CornerList = []
+    CornerListSortedXY = []
 
-        # Corner list for sorted and best corner candidates
-        CornerListSortedXY = []
+    # Corner list for sorted and best corner candidates
+    CornerListSortedXY = []
 
-        usedimgsNum.append(False)
+    usedimgsNum.append(False)
 
-        # If there is not enough coroner candidates found, then do not use the image or img points
-        if len(ObjectsH) >= CheckPoints:
-            usedimgsNum[imgcount] = True
-            enoughPointimgs = enoughPointimgs + 1
+    # If there is not enough coroner candidates found, then do not use the image or img points
+    if len(ObjectsH) >= CheckPoints:
+        usedimgsNum[imgcount] = True
+        enoughPointimgs = enoughPointimgs + 1
 
-            # Take the 54 biggest objects and make a circle around it. 54 = number of points at the checkerboard
-            for h in range(CheckPoints):
-                corner = np.array(ObjectsH[h])
-                y_min = min(corner[:, 0])
-                y_max = max(corner[:, 0])
-                x_min = min(corner[:, 1])
-                x_max = max(corner[:, 1])
+        # Take the [CheckPoints] biggest objects and make a circle around it. : CheckPoints = number of points
+        # on the checkerboard
+        for h in range(CheckPoints):
+            corner = np.array(ObjectsH[h])
+            y_min = min(corner[:, 0])
+            y_max = max(corner[:, 0])
+            x_min = min(corner[:, 1])
+            x_max = max(corner[:, 1])
 
-                # Calculate the center of mass for each object
-                xbb = int((x_min + x_max) / 2)
-                ybb = int((y_min + y_max) / 2)
+            # Calculate the center of mass for each object
+            xbb = int((x_min + x_max) / 2)
+            ybb = int((y_min + y_max) / 2)
 
-                img.itemset((ybb, xbb, 0), 255)
-                img.itemset((ybb, xbb, 1), 0)
-                img.itemset((ybb, xbb, 2), 0)
+            img.itemset((ybb, xbb, 0), 255)
+            img.itemset((ybb, xbb, 1), 0)
+            img.itemset((ybb, xbb, 2), 0)
 
-                # Draw a circle around the center
-                cv.circle(img, (xbb, ybb), 30, (255, 0, 0), thickness=2, lineType=cv.LINE_8)
+            # Draw a circle around the center
+            cv.circle(img, (xbb, ybb), 30, (255, 0, 0), thickness=2, lineType=cv.LINE_8)
 
-                CornerList.append([xbb, ybb])
+            CornerList.append([xbb, ybb])
 
-            # Sort 2D numpy array by 1st Column from high to low
-            CornerListSortedX = sorted(CornerList, key=lambda x: x[0], reverse=True)
+        # Sort 2D numpy array by 1st Column from high to low
+        CornerListSortedX = sorted(CornerList, key=lambda x: x[0], reverse=True)
 
-            # Sort 2D numpy array by 2nd Column from high to low, but only for checkerboard_size[1] at a time
-            for n in range(int(CheckPoints / checkerboard_size[1])):
-                start = n * checkerboard_size[1]
-                stop = start + checkerboard_size[1]
-                nextRows = CornerListSortedX[start:stop]
-                nextRows_sorted = sorted(nextRows, key=lambda x: x[1], reverse=True)
-                for x in range(len(nextRows_sorted)):
-                    CornerListSortedXY.append(nextRows_sorted[x])
+        # Sort 2D numpy array by 2nd Column from high to low, but only for checkerboard_size[1] at a time
+        for n in range(int(CheckPoints / checkerboard_size[1])):
+            start = n * checkerboard_size[1]
+            stop = start + checkerboard_size[1]
+            nextRows = CornerListSortedX[start:stop]
+            nextRows_sorted = sorted(nextRows, key=lambda x: x[1], reverse=True)
+            for x in range(len(nextRows_sorted)):
+                CornerListSortedXY.append(nextRows_sorted[x])
 
-            CornerListSortedXY_stacked.append(CornerListSortedXY)
+        # A new coordinate system have been created for one image, stack this in a 3 dimensional coordinate system
+        CornerListSortedXY_stacked.append(CornerListSortedXY)
 
     imgcount = imgcount + 1
 
+# Make the array a numpy array
 np_array = np.array(CornerListSortedXY_stacked)
+
+# When converting a np. array to a mat file the array shape changes. The numpy array is 2x4x2, but the .mat file needs
+# it to be 4x2x2
 np_array_switch = np.moveaxis(np_array, 0, -1)
 
+# Save the np. array in .mat format
 scipy.io.savemat('np_array_switch.mat', mdict={'np_array_switch': np_array_switch})
-
-# Some images where not used because the corners where not found
-# Therefore make a new array, where only all image points in the checkerboard where detected
-print(usedimgsNum)
-ar = 0
-for n in range(len(usedimgsNum)):
-    if n == 0 and usedimgsNum[n] == True:
-        imgspoints_print = np.zeros([CheckPoints, 2, enoughPointimgs])
-        imgspoints_print[:, :, ar] = imgspoints[:, :, n]
-        ar = ar + 1
-    elif usedimgsNum[n] == True:
-        imgspoints_print[:, :, ar] = imgspoints[:, :, n]
-        ar = ar + 1
-    else:
-        print('Delete tha first image in checkerboard_pics or replace checkerboard images')
-
-scipy.io.savemat('imgPoints.mat', mdict={'imgspoints_print': imgspoints_print})
-
-'''
-        print('Creating cornerlist file')
-        CornerFileList = open('CornerFileList', 'w')
-        CornerFileList.write('x, \t y \n')
-        for i in range(len(CornerList)):
-            CornerFileList.write(str(CornerList[i][0]) + ' , ' + str(CornerList[i][1]) + '\n')
-        CornerFileList.close()
-
-    # Create a list of Response value with the corrosponding x and y coordinate
-    if bool(test):
-        print('Creating corner file')
-
-        CornerFile = open('CornersFoundCoordniate.txt', 'w')
-        CornerFile.write('x, \t y, \t Ix, \t Iy, \t R \n')
-        for i in range(len(CornerCoordinate)):
-            CornerFile.write(str(CornerCoordinate[i][0]) + ' , ' + str(CornerCoordinate[i][1]) + ' , ' + str(CornerCoordinate[i][2]) + ' , ' + str(CornerCoordinate[i][3]) + ' , ' + str(CornerCoordinate[i][4]) + '\n')
-        CornerFile.close()
-
-        scipy.io.savemat('imgpoint.mat', mdict={'CornerCoordinate': CornerCoordinate})
-    '''
 
 print('Done!')
 
